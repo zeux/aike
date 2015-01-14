57ce07481bf5dcfc02cf4888a0c54426fd504cce
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2015-01-13 21:34:55 -0800

    compiler: Remove redundant LLVM includes
    Also add <algorithm> and simplify the test

--- compiler/common.hpp
+++ compiler/common.hpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <string>
 #include <memory>
+#include <algorithm>
 
 #include <unordered_map>
 
--- compiler/compiler.cpp
+++ compiler/compiler.cpp
@@ -2,8 +2,6 @@
 #include "lexer.hpp"
 #include "output.hpp"
 
-#include "llvm/IR/Verifier.h"
-#include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
