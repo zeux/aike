1a50b50b7d396dff0a241168b28b64b21d7d0859
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2015-02-11 22:24:37 -0800

    runtime: Add aike_newarr

--- runtime/memory.cpp
+++ runtime/memory.cpp
@@ -3,5 +3,12 @@
 
 void* aike_new(size_t size)
 {
+	// TODO: panic on null result
 	return malloc(size);
+}
+
+void* aike_newarr(size_t count, size_t size)
+{
+	// TODO: panic on overflow
+	return aike_new(count * size);
 }
--- runtime/memory.hpp
+++ runtime/memory.hpp
@@ -1,3 +1,4 @@
 #pragma once
 
 AIKE_EXTERN void* aike_new(size_t size);
+AIKE_EXTERN void* aike_newarr(size_t count, size_t size);
