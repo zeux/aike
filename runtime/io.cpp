a59259f1358264a829ee6943633a6338733fbcae
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2015-02-17 19:21:20 -0800

    runtime: Add printfn function
    It takes AikeString so we don't have to "skip" the extra size argument.
    Also, who needs loops when you have recursion?

--- runtime/io.cpp
+++ runtime/io.cpp
@@ -1,6 +1,22 @@
 #include "common.hpp"
 
+#include <cstdarg>
+
 AIKE_EXTERN void println(AikeString str)
 {
 	printf("%.*s\n", int(str.size), str.data);
+}
+
+AIKE_EXTERN void printfn(AikeString format, ...)
+{
+	char* buffer = static_cast<char*>(alloca(format.size + 2));
+
+	memcpy(buffer, format.data, format.size);
+	buffer[format.size + 0] = '\n';
+	buffer[format.size + 1] = 0;
+
+	va_list args;
+	va_start(args, format);
+	vprintf(buffer, args);
+	va_end(args);
 }
