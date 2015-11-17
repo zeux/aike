d97838a2d806aec476508ed58bc86e6464f43876
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2015-11-17 00:58:32 -0800

    copmiler: Implement stackless Ast traversal
    Use an explicit stack to simplify profiling (and for slightly faster traversal).

--- compiler/visit.cpp
+++ compiler/visit.cpp
@@ -3,103 +3,123 @@
 
 #include "ast.hpp"
 
+static void visitAstStackless(Ast* root, function<bool (Ast*)> f, Ast* ignore = nullptr)
+{
+	vector<Ast*> stack;
+	stack.push_back(root);
+
+	while (!stack.empty())
+	{
+		Ast* node = stack.back();
+		stack.pop_back();
+
+		if (node != ignore && f(node))
+			continue;
+
+		size_t offset = stack.size();
+
+		if (UNION_CASE(LiteralArray, n, node))
+		{
+			for (auto& c: n->elements)
+				stack.push_back(c);
+		}
+		else if (UNION_CASE(LiteralStruct, n, node))
+		{
+			for (auto& c: n->fields)
+				stack.push_back(c.second);
+		}
+		else if (UNION_CASE(Member, n, node))
+		{
+			stack.push_back(n->expr);
+		}
+		else if (UNION_CASE(Block, n, node))
+		{
+			for (auto& c: n->body)
+				stack.push_back(c);
+		}
+		else if (UNION_CASE(Module, n, node))
+		{
+			stack.push_back(n->body);
+		}
+		else if (UNION_CASE(Call, n, node))
+		{
+			stack.push_back(n->expr);
+
+			for (auto& a: n->args)
+				stack.push_back(a);
+		}
+		else if (UNION_CASE(Unary, n, node))
+		{
+			stack.push_back(n->expr);
+		}
+		else if (UNION_CASE(Binary, n, node))
+		{
+			stack.push_back(n->left);
+			stack.push_back(n->right);
+		}
+		else if (UNION_CASE(Index, n, node))
+		{
+			stack.push_back(n->expr);
+			stack.push_back(n->index);
+		}
+		else if (UNION_CASE(Assign, n, node))
+		{
+			stack.push_back(n->left);
+			stack.push_back(n->right);
+		}
+		else if (UNION_CASE(If, n, node))
+		{
+			stack.push_back(n->cond);
+			stack.push_back(n->thenbody);
+
+			if (n->elsebody)
+				stack.push_back(n->elsebody);
+		}
+		else if (UNION_CASE(For, n, node))
+		{
+			stack.push_back(n->expr);
+			stack.push_back(n->body);
+		}
+		else if (UNION_CASE(While, n, node))
+		{
+			stack.push_back(n->expr);
+			stack.push_back(n->body);
+		}
+		else if (UNION_CASE(FnDecl, n, node))
+		{
+			if (n->body)
+				stack.push_back(n->body);
+		}
+		else if (UNION_CASE(Fn, n, node))
+		{
+			stack.push_back(n->decl);
+		}
+		else if (UNION_CASE(VarDecl, n, node))
+		{
+			stack.push_back(n->expr);
+		}
+		else if (UNION_CASE(TyDecl, n, node))
+		{
+			if (UNION_CASE(Struct, t, n->def))
+			{
+				for (auto& c: t->fields)
+					if (c.expr)
+						stack.push_back(c.expr);
+			}
+		}
+
+		reverse(stack.begin() + offset, stack.end());
+	}
+}
+
 void visitAst(Ast* node, function<bool (Ast*)> f)
 {
-	if (!f(node))
-		visitAstInner(node, f);
+	visitAstStackless(node, f);
 }
 
 void visitAstInner(Ast* node, function<bool (Ast*)> f)
 {
-	if (UNION_CASE(LiteralArray, n, node))
-	{
-		for (auto& c: n->elements)
-			visitAst(c, f);
-	}
-	else if (UNION_CASE(LiteralStruct, n, node))
-	{
-		for (auto& c: n->fields)
-			visitAst(c.second, f);
-	}
-	else if (UNION_CASE(Member, n, node))
-	{
-		visitAst(n->expr, f);
-	}
-	else if (UNION_CASE(Block, n, node))
-	{
-		for (auto& c: n->body)
-			visitAst(c, f);
-	}
-	else if (UNION_CASE(Module, n, node))
-	{
-		visitAst(n->body, f);
-	}
-	else if (UNION_CASE(Call, n, node))
-	{
-		visitAst(n->expr, f);
-
-		for (auto& a: n->args)
-			visitAst(a, f);
-	}
-	else if (UNION_CASE(Unary, n, node))
-	{
-		visitAst(n->expr, f);
-	}
-	else if (UNION_CASE(Binary, n, node))
-	{
-		visitAst(n->left, f);
-		visitAst(n->right, f);
-	}
-	else if (UNION_CASE(Index, n, node))
-	{
-		visitAst(n->expr, f);
-		visitAst(n->index, f);
-	}
-	else if (UNION_CASE(Assign, n, node))
-	{
-		visitAst(n->left, f);
-		visitAst(n->right, f);
-	}
-	else if (UNION_CASE(If, n, node))
-	{
-		visitAst(n->cond, f);
-		visitAst(n->thenbody, f);
-
-		if (n->elsebody)
-			visitAst(n->elsebody, f);
-	}
-	else if (UNION_CASE(For, n, node))
-	{
-		visitAst(n->expr, f);
-		visitAst(n->body, f);
-	}
-	else if (UNION_CASE(While, n, node))
-	{
-		visitAst(n->expr, f);
-		visitAst(n->body, f);
-	}
-	else if (UNION_CASE(FnDecl, n, node))
-	{
-		if (n->body)
-			visitAst(n->body, f);
-	}
-	else if (UNION_CASE(Fn, n, node))
-	{
-		visitAst(n->decl, f);
-	}
-	else if (UNION_CASE(VarDecl, n, node))
-	{
-		visitAst(n->expr, f);
-	}
-	else if (UNION_CASE(TyDecl, n, node))
-	{
-		if (UNION_CASE(Struct, t, n->def))
-		{
-			for (auto& c: t->fields)
-				if (c.expr)
-					visitAst(c.expr, f);
-		}
-	}
+	visitAstStackless(node, f, /* ignore= */ node);
 }
 
 void visitAstTypes(Ast* node, function<void (Ty*)> f)
